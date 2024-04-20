3732d909945a01241d43b68afe4ba9d2b2c94781
escalopa <ahmad.helaly.dev@gmail.com>
2024-04-20 15:27:11 +0300

    feat: add benchmark tests

---
+++ cache/bench_test.go
@@ -0,0 +1,238 @@
+package cache_test
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/catalystgo/kache/cache"
+	"github.com/catalystgo/kache/cache/arc"
+	"github.com/catalystgo/kache/cache/lru"
+	"github.com/catalystgo/kache/cache/ristretto"
+	"github.com/catalystgo/kache/cache/twoqueue"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	// "github.com/catalystgo/realtime-config-go/helper"
+)
+
+const (
+	name       = "cache"
+	capValue   = 100000
+	ttlKey     = "ttlValue"
+	capKey     = "capValue"
+	ttlValue   = 1 * time.Minute
+	maxAvgTime = time.Second
+)
+
+var (
+	random   rand.Source
+	testdata = make(map[interface{}]interface{}, capValue)
+)
+
+func init() {
+	for i := 0; i < capValue; i++ {
+		testdata[i] = i
+	}
+	random = rand.NewSource(time.Now().UTC().UnixNano())
+}
+
+func warmUp(cache cache.Cache) {
+	for k, v := range testdata {
+		cache.Put(k, v)
+	}
+}
+
+func createLRU(t testing.TB) *lru.Cache {
+	c, err := lru.NewCache(name, capValue, ttlValue)
+	require.NoError(t, err)
+	return c
+}
+
+func createARC(t testing.TB) *arc.Cache {
+	c, err := arc.NewCache(name, capValue, ttlValue)
+	require.NoError(t, err)
+	return c
+}
+
+func create2Q(t testing.TB) *twoqueue.Cache {
+	c, err := twoqueue.NewCache(name, capValue, ttlValue)
+	require.NoError(t, err)
+	return c
+}
+
+func createRistretto(t testing.TB) *ristretto.Cache {
+	c, err := ristretto.New(name, capValue, ttlValue)
+	require.NoError(t, err)
+	return c
+}
+
+// func createRealtime(t testing.TB, opts ...realtime.CacheOption) *realtime.Cache {
+// 	ctrl := gomock.NewController(t)
+// 	defer ctrl.Finish()
+// 	client := NewMockClient(ctrl)
+// 	client.EXPECT().
+// 		Value(gomock.Any(), capKey).Return(helper.NewValue(capValue), nil).AnyTimes()
+// 	client.EXPECT().
+// 		Value(gomock.Any(), ttlKey).Return(helper.NewValue(ttlValue), nil).AnyTimes()
+// 	client.EXPECT().
+// 		WatchVariable(gomock.Any(), gomock.Any(), gomock.Any()).MaxTimes(1).MinTimes(1)
+
+// 	c, err := realtime.NewCache(client, name, capKey, ttlKey, opts...)
+// 	require.NoError(t, err)
+// 	return c
+// }
+
+func load(op func(), rps int) (stop chan struct{}) {
+	stop = make(chan struct{})
+	go func() {
+		t, n, tc := time.Now(), 0, 0
+		for {
+			go func() {
+				op()
+			}()
+			n++
+			tc++
+			if n == rps {
+				t, n = time.Now(), 0
+			}
+			select {
+			case <-stop:
+				return
+			default:
+
+				time.Sleep(time.Until(t.Add(time.Second)) / time.Duration(rps-n))
+				continue
+			}
+		}
+	}()
+
+	time.Sleep(1 * time.Second)
+	return stop
+}
+
+func cacheOp(c cache.Cache, hitRate int) {
+	v := rand.Intn(capValue)      //nolint:gosec
+	if rand.Intn(100) < hitRate { //nolint:gosec
+		c.Get(v)
+	} else {
+		c.Put(v, v)
+	}
+}
+
+func bench(t testing.TB, op func()) time.Duration {
+	var total, i, timeout time.Duration = 0, 0, 3 * time.Second
+	start := time.Now()
+	for {
+		s := time.Now()
+		op()
+		d := time.Since(s)
+		total += d
+		i++
+		if time.Since(start) > timeout {
+			break
+		}
+	}
+	avg := total / time.Duration(i)
+	t.Logf("%s\t%d ns/op", t.Name(), avg)
+
+	return avg
+}
+
+func benchCache(method string, cache cache.Cache, rps int, hitrate int) func(t *testing.T) {
+	return func(t *testing.T) {
+		warmUp(cache)
+		l := load(func() { cacheOp(cache, hitrate) }, rps)
+		defer close(l)
+
+		got, exp := bench(t, func() {
+			switch method {
+			case "Get":
+				cache.Get(rand.Intn(capValue)) //nolint:gosec
+			case "Put":
+				v := rand.Intn(capValue) //nolint:gosec
+				cache.Put(v, v)
+			default:
+				t.Fatalf("unknown method %q", method)
+			}
+		}), maxAvgTime
+
+		assert.Truef(t, got < exp, "expected avg op time < %s, got: %s", exp, got)
+	}
+}
+
+func TestBenchmarks(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+	t.Run("LRU Get under load RPS 200k HitRate 90", benchCache("Get", createLRU(t), 200000, 90))
+	t.Run("LRU Get under load RPS 400k HitRate 90", benchCache("Get", createLRU(t), 400000, 90))
+	t.Run("LRU Get under load RPS 200k HitRate 10", benchCache("Get", createLRU(t), 200000, 10))
+	t.Run("LRU Get under load RPS 400k HitRate 10", benchCache("Get", createLRU(t), 400000, 10))
+	t.Run("LRU Put under load RPS 200k HitRate 90", benchCache("Put", createLRU(t), 200000, 90))
+	t.Run("LRU Put under load RPS 400k HitRate 90", benchCache("Put", createLRU(t), 400000, 90))
+	t.Run("LRU Put under load RPS 200k HitRate 10", benchCache("Put", createLRU(t), 200000, 10))
+	t.Run("LRU Put under load RPS 400k HitRate 10", benchCache("Put", createLRU(t), 400000, 10))
+
+	t.Run("ARC Get under load RPS 200k HitRate 90", benchCache("Get", createARC(t), 200000, 90))
+	t.Run("ARC Get under load RPS 400k HitRate 90", benchCache("Get", createARC(t), 400000, 90))
+	t.Run("ARC Get under load RPS 200k HitRate 10", benchCache("Get", createARC(t), 200000, 10))
+	t.Run("ARC Get under load RPS 400k HitRate 10", benchCache("Get", createARC(t), 400000, 10))
+	t.Run("ARC Put under load RPS 200k HitRate 90", benchCache("Put", createARC(t), 200000, 90))
+	t.Run("ARC Put under load RPS 400k HitRate 90", benchCache("Put", createARC(t), 400000, 90))
+	t.Run("ARC Put under load RPS 200k HitRate 10", benchCache("Put", createARC(t), 200000, 10))
+	t.Run("ARC Put under load RPS 400k HitRate 10", benchCache("Put", createARC(t), 400000, 10))
+
+	t.Run("2Q Get under load RPS 200k HitRate 90", benchCache("Get", create2Q(t), 200000, 90))
+	t.Run("2Q Get under load RPS 400k HitRate 90", benchCache("Get", create2Q(t), 400000, 90))
+	t.Run("2Q Get under load RPS 200k HitRate 10", benchCache("Get", create2Q(t), 200000, 10))
+	t.Run("2Q Get under load RPS 400k HitRate 10", benchCache("Get", create2Q(t), 400000, 10))
+	t.Run("2Q Put under load RPS 200k HitRate 90", benchCache("Put", create2Q(t), 200000, 90))
+	t.Run("2Q Put under load RPS 400k HitRate 90", benchCache("Put", create2Q(t), 400000, 90))
+	t.Run("2Q Put under load RPS 200k HitRate 10", benchCache("Put", create2Q(t), 200000, 10))
+	t.Run("2Q Put under load RPS 400k HitRate 10", benchCache("Put", create2Q(t), 400000, 10))
+
+	t.Run("Ristretto Get under load RPS 200k HitRate 90", benchCache("Get", createRistretto(t), 200000, 90))
+	t.Run("Ristretto Get under load RPS 400k HitRate 90", benchCache("Get", createRistretto(t), 400000, 90))
+	t.Run("Ristretto Get under load RPS 200k HitRate 10", benchCache("Get", createRistretto(t), 200000, 10))
+	t.Run("Ristretto Get under load RPS 400k HitRate 10", benchCache("Get", createRistretto(t), 400000, 10))
+	t.Run("Ristretto Put under load RPS 200k HitRate 90", benchCache("Put", createRistretto(t), 200000, 90))
+	t.Run("Ristretto Put under load RPS 400k HitRate 90", benchCache("Put", createRistretto(t), 400000, 90))
+	t.Run("Ristretto Put under load RPS 200k HitRate 10", benchCache("Put", createRistretto(t), 200000, 10))
+	t.Run("Ristretto Put under load RPS 400k HitRate 10", benchCache("Put", createRistretto(t), 400000, 10))
+
+	// t.Run("Realtime with LRU engine Get under load RPS 200k HitRate 90", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.EngineLRU)), 200000, 90))
+	// t.Run("Realtime with LRU engine Get under load RPS 400k HitRate 90", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.EngineLRU)), 400000, 90))
+	// t.Run("Realtime with LRU engine Get under load RPS 200k HitRate 10", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.EngineLRU)), 200000, 10))
+	// t.Run("Realtime with LRU engine Get under load RPS 400k HitRate 10", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.EngineLRU)), 400000, 10))
+	// t.Run("Realtime with LRU engine Put under load RPS 200k HitRate 90", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.EngineLRU)), 200000, 90))
+	// t.Run("Realtime with LRU engine Put under load RPS 400k HitRate 90", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.EngineLRU)), 400000, 90))
+	// t.Run("Realtime with LRU engine Put under load RPS 200k HitRate 10", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.EngineLRU)), 200000, 10))
+	// t.Run("Realtime with LRU engine Put under load RPS 400k HitRate 10", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.EngineLRU)), 400000, 10))
+
+	// t.Run("Realtime with ARC engine Get under load RPS 200k HitRate 90", benchCache("Get", createRealtime(t), 200000, 90))
+	// t.Run("Realtime with ARC engine Get under load RPS 400k HitRate 90", benchCache("Get", createRealtime(t), 400000, 90))
+	// t.Run("Realtime with ARC engine Get under load RPS 200k HitRate 10", benchCache("Get", createRealtime(t), 200000, 10))
+	// t.Run("Realtime with ARC engine Get under load RPS 400k HitRate 10", benchCache("Get", createRealtime(t), 400000, 10))
+	// t.Run("Realtime with ARC engine Put under load RPS 200k HitRate 90", benchCache("Put", createRealtime(t), 200000, 90))
+	// t.Run("Realtime with ARC engine Put under load RPS 400k HitRate 90", benchCache("Put", createRealtime(t), 400000, 90))
+	// t.Run("Realtime with ARC engine Put under load RPS 200k HitRate 10", benchCache("Put", createRealtime(t), 200000, 10))
+	// t.Run("Realtime with ARC engine Put under load RPS 400k HitRate 10", benchCache("Put", createRealtime(t), 400000, 10))
+
+	// t.Run("Realtime with 2Q engine Get under load RPS 200k HitRate 90", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.Engine2Q)), 200000, 90))
+	// t.Run("Realtime with 2Q engine Get under load RPS 400k HitRate 90", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.Engine2Q)), 400000, 90))
+	// t.Run("Realtime with 2Q engine Get under load RPS 200k HitRate 10", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.Engine2Q)), 200000, 10))
+	// t.Run("Realtime with 2Q engine Get under load RPS 400k HitRate 10", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.Engine2Q)), 400000, 10))
+	// t.Run("Realtime with 2Q engine Put under load RPS 200k HitRate 90", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.Engine2Q)), 200000, 90))
+	// t.Run("Realtime with 2Q engine Put under load RPS 400k HitRate 90", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.Engine2Q)), 400000, 90))
+	// t.Run("Realtime with 2Q engine Put under load RPS 200k HitRate 10", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.Engine2Q)), 200000, 10))
+	// t.Run("Realtime with 2Q engine Put under load RPS 400k HitRate 10", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.Engine2Q)), 400000, 10))
+
+	// t.Run("Realtime with Ristretto engine Get under load RPS 200k HitRate 90", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.EngineRistretto)), 200000, 90))
+	// t.Run("Realtime with Ristretto engine Get under load RPS 400k HitRate 90", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.EngineRistretto)), 400000, 90))
+	// t.Run("Realtime with Ristretto engine Get under load RPS 200k HitRate 10", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.EngineRistretto)), 200000, 10))
+	// t.Run("Realtime with Ristretto engine Get under load RPS 400k HitRate 10", benchCache("Get", createRealtime(t, realtime.WithEngine(realtime.EngineRistretto)), 400000, 10))
+	// t.Run("Realtime with Ristretto engine Put under load RPS 200k HitRate 90", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.EngineRistretto)), 200000, 90))
+	// t.Run("Realtime with Ristretto engine Put under load RPS 400k HitRate 90", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.EngineRistretto)), 400000, 90))
+	// t.Run("Realtime with Ristretto engine Put under load RPS 200k HitRate 10", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.EngineRistretto)), 200000, 10))
+	// t.Run("Realtime with Ristretto engine Put under load RPS 400k HitRate 10", benchCache("Put", createRealtime(t, realtime.WithEngine(realtime.EngineRistretto)), 400000, 10))
+}
---
+++ cache/parallel_bench_test.go
@@ -0,0 +1,111 @@
+package cache_test
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/catalystgo/kache/cache"
+	"github.com/catalystgo/kache/cache/arc"
+	"github.com/catalystgo/kache/cache/lru"
+	"github.com/catalystgo/kache/cache/ristretto"
+)
+
+func benchmarkParallelGet(b *testing.B, c cache.Cache, workers, iterations int) {
+	keys := make([][]int, workers)
+	for i := 0; i < workers; i++ {
+		keys[i] = make([]int, iterations)
+		for j := 0; j < iterations; j++ {
+			keys[i][j] = rand.Intn(capValue) //nolint:gosec
+		}
+	}
+
+	warmUp(c)
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		var wg sync.WaitGroup
+		wg.Add(workers)
+		for i := 0; i < workers; i++ {
+			workerKeys := keys[i]
+			go func() {
+				defer wg.Done()
+
+				for j := 0; j < iterations; j++ {
+					c.Get(workerKeys[j])
+				}
+			}()
+		}
+
+		wg.Wait()
+	}
+}
+
+// pkg: github.com/catalystgo/kache/cache
+// BenchmarkParallelGet/*lru.Cache_1-12                  16          65941646 ns/op          800095 B/op      99998 allocs/op
+// BenchmarkParallelGet/*lru.Cache_4-12                   4         264020145 ns/op         3200220 B/op     399999 allocs/op
+// BenchmarkParallelGet/*lru.Cache_8-12                   2         565829862 ns/op         6402508 B/op     800002 allocs/op
+// BenchmarkParallelGet/*lru.Cache_16-12                  1        1079725471 ns/op        12806416 B/op    1600015 allocs/op
+// BenchmarkParallelGet/*lru.Cache_32-12                  1        2143424758 ns/op        25620880 B/op    3200040 allocs/op
+// BenchmarkParallelGet/*arc.Cache_1-12                  18          64991280 ns/op          800918 B/op     100000 allocs/op
+// BenchmarkParallelGet/*arc.Cache_4-12                   4         258310478 ns/op         3200100 B/op     399999 allocs/op
+// BenchmarkParallelGet/*arc.Cache_8-12                   2         554299090 ns/op         6401336 B/op     799997 allocs/op
+// BenchmarkParallelGet/*arc.Cache_16-12                  1        1215714909 ns/op        28893168 B/op    1804020 allocs/op
+// BenchmarkParallelGet/*arc.Cache_32-12                  1        2328896713 ns/op        41700416 B/op    3404009 allocs/op
+// BenchmarkParallelGet/*ristretto.Cache_1-12            26          39008821 ns/op         1597117 B/op     101556 allocs/op
+// BenchmarkParallelGet/*ristretto.Cache_4-12            16          62995929 ns/op         6251269 B/op     405958 allocs/op
+// BenchmarkParallelGet/*ristretto.Cache_8-12            13          87404013 ns/op        11226728 B/op     809420 allocs/op
+// BenchmarkParallelGet/*ristretto.Cache_16-12            6         190891075 ns/op        13428126 B/op    1601195 allocs/op
+// BenchmarkParallelGet/*ristretto.Cache_32-12            3         384424426 ns/op        26532002 B/op    3201796 allocs/op
+func BenchmarkParallelGet(b *testing.B) {
+	lruFactory := func() cache.NamedCache {
+		c, _ := lru.NewCache("data", capValue, time.Minute)
+
+		return c
+	}
+
+	arcFactory := func() cache.NamedCache {
+		c, _ := arc.NewCache("data", capValue, time.Minute)
+
+		return c
+	}
+
+	ristrettoFactory := func() cache.NamedCache {
+		c, _ := ristretto.New("ristretto", capValue, time.Minute)
+
+		return c
+	}
+
+	cases := []struct {
+		c          cache.NamedCache
+		workers    int
+		iterations int
+	}{
+		{c: lruFactory(), workers: 1, iterations: 100_000},
+		{c: lruFactory(), workers: 4, iterations: 100_000},
+		{c: lruFactory(), workers: 8, iterations: 100_000},
+		{c: lruFactory(), workers: 16, iterations: 100_000},
+		{c: lruFactory(), workers: 32, iterations: 100_000},
+
+		{c: arcFactory(), workers: 1, iterations: 100_000},
+		{c: arcFactory(), workers: 4, iterations: 100_000},
+		{c: arcFactory(), workers: 8, iterations: 100_000},
+		{c: arcFactory(), workers: 16, iterations: 100_000},
+		{c: arcFactory(), workers: 32, iterations: 100_000},
+
+		{c: ristrettoFactory(), workers: 1, iterations: 100_000},
+		{c: ristrettoFactory(), workers: 4, iterations: 100_000},
+		{c: ristrettoFactory(), workers: 8, iterations: 100_000},
+		{c: ristrettoFactory(), workers: 16, iterations: 100_000},
+		{c: ristrettoFactory(), workers: 32, iterations: 100_000},
+	}
+
+	for _, tc := range cases {
+		b.Run(fmt.Sprintf("%T_%v", tc.c, tc.workers), func(b *testing.B) {
+			benchmarkParallelGet(b, tc.c, tc.workers, tc.iterations)
+		})
+	}
+}
